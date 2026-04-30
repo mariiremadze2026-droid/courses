4749d6a1d2177145546b757bc31a03620b0ad542
mariiremadze2026-droid <mari.iremadze2026@icloud.com>
2026-04-30 10:45:31 +0400

    Delete particles-config.js

--- particles-config.js
+++
@@ -1,8 +0,0 @@
-tsParticles.load("tsparticles", {
-    particles: {
-        number: { value: 50 },
-        color: { value: "#8b5cf6" },
-        links: { enable: true, color: "#8b5cf6", opacity: 0.3 },
-        move: { enable: true, speed: 1 }
-    }
-});
